74badb4f8c2e634ef32d6c3f2638a063b3aae7db
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-04-29 22:58:47 +0530

    Updating shell-scripting

---
+++ devops/special-variables.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo script name is $0
+
+echo -e "\n First Argument is " $1
+echo -e "\n second Argument is " $2
+echo -e "\n third Argument is " $3
+
+echo -e "\n All arguments are " $@
+
+echo -e "\n Number of arguments is " $#
