6f3e3fbfb4686c375bbacecb44a194d3fc17ab44
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-04-29 14:40:21 +0530

    col

--- devops/print.sh
+++ devops/print.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 echo -e "dileep\nchandra\nsony\n\n"
-echo 'dileep\nchandra\nsony\n\n'
+echo 'dileep\nchandra\nsony'
+
+echo -e "\n\n"
 
 echo -e "\e[31mDileep\e[0m"
 echo -e "\e[34mchandra\e[0m"
 echo -e "\e[32msony\e[0m"
+
+
