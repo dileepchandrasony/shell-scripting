6a3311748bd45937658a598f69054b6e1ebd0d99
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-04-29 22:27:57 +0530

    Updating shell-scripting

--- devops/variables.sh
+++ devops/variables.sh
@@ -9,3 +9,9 @@ date=$(date +%F)
 echo "today's date is $date"
 
 echo value of variable from CLI is ${A}
+
+ARY=(1000 2000 3000)
+
+echo first item in the array list is ${ARY[0]}
+echo second item in the array list is ${ARY[1]}
+echo third item in the array list is ${ARY[2]}
