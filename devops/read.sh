081405dc5bdfcbc7b851a95fcbb8f4146797d7ee
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-04-29 22:54:58 +0530

    Updating shell-scripting

--- devops/read.sh
+++ devops/read.sh
@@ -4,6 +4,6 @@ read -p "Enter your name :" name
 read -p "Enter your gender :" gender
 read -p "Enter your age :" age
 
-echo -e "\n\n Your name is $name"
+echo -e "\n\nYour name is $name"
 echo "Your gender is $gender"
 echo "Your age is $age"
